40600acd12ad2872d4a3def9e4d6f2e34c008887
phidtech <phidtechnology@gmail.com>
2026-01-28 08:13:13 +0300

    Add SuperAdmin seeder for phidtechnology@gmail.com

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class SuperAdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'phidtechnology@gmail.com'],
+            [
+                'name' => 'Super Admin',
+                'email' => 'phidtechnology@gmail.com',
+                'password' => Hash::make('Dativa@@5006'),
+                'role' => 'admin',
+                'is_active' => true,
+                'email_verified_at' => now(),
+                'sms_credits' => 0,
+            ]
+        );
+    }
+}
